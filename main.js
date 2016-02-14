0ff9ef06dbc9ce0a7e5a82e845b92284e3375861
Matt Gibbs <matt.gibbs@hpe.com>
2016-02-14 12:57:06 -0500

    Randomly pick a file from list to print into the code-hero

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 $(function() {
 
+  var files = [
+    'https://raw.githubusercontent.com/mtgibbs/chartist-plugin-labelclasses/master/src/scripts/chartist-plugin-labelclasses.js',
+    'https://raw.githubusercontent.com/mtgibbs/gulp-less-branding-js/master/index.js',
+    'https://raw.githubusercontent.com/mtgibbs/chartist-plugin-barlabels/master/src/scripts/chartist-plugin-barlabels.js',
+    'https://raw.githubusercontent.com/mtgibbs/estudu/master/js/main.js',
+    'https://raw.githubusercontent.com/mtgibbs/inkscape-stroke-to-path/master/stroke-to-path.ps1'
+  ];
 
-  $.get('https://raw.githubusercontent.com/mtgibbs/gulp-less-branding-js/master/index.js').done(function(response) {
-    response = response.replace(/(\n)+/g, '<br />');
-    var strings = [];
 
-    strings.push(response);
+
+  $.get(files[Math.floor(Math.random() * files.length)]).done(function(response) {
+    response = response.replace(/(\n)+/g, '   ');
+
     $('#typingThing').typed({
-      strings: strings,
-      typeSpeed: 1
+      strings: [response],
+      typeSpeed: 1,
+      contentType: 'text',
+      onStringTyped: function() {
+        var $elem = $('.code-typing-container');
+        $elem.scrollTop($elem[0].scrollHeight);
+      }
     });
   });
 });
