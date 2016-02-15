108a28233efa22efcf51932ec6dbdabc392a7020
Matt Gibbs <matt.gibbs@hpe.com>
2016-02-14 23:28:54 -0500

    Whitespaze

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ $(function() {
     'https://raw.githubusercontent.com/mtgibbs/inkscape-stroke-to-path/master/stroke-to-path.ps1'
   ];
 
+
   $.get(files[Math.floor(Math.random() * files.length)]).done(function(response) {
     response = response.replace(/(\n)+/g, '   ');
 
