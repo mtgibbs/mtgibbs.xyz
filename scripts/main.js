05a288015b72e74d4ad75cb1c01f1176423c1ca3
Matt Gibbs <matt.gibbs@hpe.com>
2016-02-15 23:00:14 -0500

    Removing unused listener function.
    Also removed the powershell script from the code hero

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,10 @@
 $(function() {
-
   var files = [
     'https://raw.githubusercontent.com/mtgibbs/chartist-plugin-labelclasses/master/src/scripts/chartist-plugin-labelclasses.js',
     'https://raw.githubusercontent.com/mtgibbs/gulp-less-branding-js/master/index.js',
     'https://raw.githubusercontent.com/mtgibbs/chartist-plugin-barlabels/master/src/scripts/chartist-plugin-barlabels.js',
     'https://raw.githubusercontent.com/mtgibbs/estudu/master/js/main.js',
-    'https://raw.githubusercontent.com/mtgibbs/inkscape-stroke-to-path/master/stroke-to-path.ps1'
   ];
-
 
   $.get(files[Math.floor(Math.random() * files.length)]).done(function(response) {
     response = response.replace(/(\n)+/g, '   ');
@@ -15,11 +12,7 @@ $(function() {
     $('#typingThing').typed({
       strings: [response],
       typeSpeed: 1,
-      contentType: 'text',
-      onStringTyped: function() {
-        var $elem = $('.code-typing-container');
-        $elem.scrollTop($elem[0].scrollHeight);
-      }
+      contentType: 'text'
     });
   });
 });
