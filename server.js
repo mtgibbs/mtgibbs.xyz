0fc14fd2e4afc99e4ba0afcf55e064e0d0147411
Matt Gibbs <matt.gibbs@hpe.com>
2016-02-15 20:58:54 -0500

    Convert to jade templating

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var app = express();
 app.use('/script', express.static(__dirname + '/dist/js'));
 app.use('/style', express.static(__dirname + '/dist/css'));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.set('view engine', 'jade');
+
+// only route available right now
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
 var port = process.env.PORT || 8081;
-
 app.listen(port, function() {
   console.log('App listening on port :' + port);
 });
