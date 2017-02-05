d23d189b4a2ed1d7032e1f6d0b42ab3e892c99fd
Matt Gibbs <matt.gibbs@hpe.com>
2017-02-05 10:12:01 -0500

    Created an InfoCard component

---
+++ src/components/infocard.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export interface InfoCardProps {
+    title: string,
+    uri: string,
+    iconClass: string,
+    body: string
+}
+
+export class InfoCard extends React.Component<InfoCardProps, undefined> {
+    render() {
+        return <div className='col-md-4'>
+            <a href={this.props.uri}>
+                <i className={this.props.iconClass}></i>
+            </a>
+            <h2>{this.props.title}</h2>
+            <p>{this.props.body}</p>
+        </div>
+    }
+}
