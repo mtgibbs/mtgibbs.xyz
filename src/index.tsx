ad6389186c3f45067c673b636524fc159d1548e8
Matt Gibbs <matt.gibbs@hpe.com>
2017-02-05 11:21:06 -0500

    Test the info card component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import {InfoCard} from './components/infocard';
 
 import '../style/style.scss';
 
 ReactDOM.render(
+    <InfoCard
+        body="test"
+        title="banana"
+        uri="https://github.com/mtgibbs"
+        iconClass="fa fa-code" />,
+    document.getElementById('app')
 );
