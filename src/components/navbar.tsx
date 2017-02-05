4c45ff1f52ca182fc939252d18e770794a7b1dce
Matt Gibbs <matt.gibbs@hpe.com>
2017-02-05 11:49:22 -0500

    Making navbar components

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,66 @@
 import * as React from 'react';
 
+interface NavbarLinkProps {
+    text: string;
+    iconClass: string;
+    uri: string;
+}
+
+class NavbarLinkListItem extends React.Component<NavbarLinkProps, undefined> {
+    render() {
+        return <li>
+            <a href={this.props.uri}>
+                {this.props.text} <i className={this.props.iconClass}></i>
+            </a>
+        </li>
+    }
+}
+
+interface NavbarLinkListProps {
+    id: string;
+}
+
+class NavbarLinkList extends React.Component<NavbarLinkListProps, undefined> {
+
+    private _links: Array<NavbarLinkListItem> = [
+        new NavbarLinkListItem({
+            text: 'Github',
+            uri: 'https://github.com/mtgibbs',
+            iconClass: 'fa fa-github-square'
+        }),
+        new NavbarLinkListItem({
+            text: 'LinkedIn',
+            uri: 'https://www.linkedin.com/in/mtgibbs21',
+            iconClass: 'fa fa-linkedin-square'
+        }),
+        new NavbarLinkListItem({
+            text: 'Email',
+            uri: 'mailto:matt@mtgibbs.xyz',
+            iconClass: 'fa fa-envelope-square'
+        }),
+    ];
+
+    render() {
+        return <div id={this.props.id} className="navbar-collapse collapse">
+            <ul className="nav navbar-nav navbar-right">
+                {this._links.map(item => { return item.render() })}
+            </ul>
+        </div>
+    }
+}
 
 export class NavBar extends React.Component<any, undefined> {
     render() {
         return <nav className="navbar navbar-default navbar-dark">
             <div className="container-fluid">
-                <div className="">
+                <div className="navbar-header">
                     <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar"></button>
                     <span className="sr-only"></span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
                 </div>
-
+                <NavbarLinkList id="navbar" />
             </div>
         </nav>
     }
