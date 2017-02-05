62f214c5f04e89c41bfd60292e06e1bb292bafcc
Matt Gibbs <matt.gibbs@hpe.com>
2017-02-05 12:12:31 -0500

    Commenting out minification for the time being

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const config = {
         ]
     },
     plugins: [
-        new Webpack.optimize.UglifyJsPlugin(),
+        // new Webpack.optimize.UglifyJsPlugin(),
         new Webpack.optimize.OccurrenceOrderPlugin(),
         new ExtractTextPlugin({
             filename: 'style.css',
