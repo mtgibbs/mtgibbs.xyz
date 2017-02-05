991d515cf4bda3e0b3277bf0c455f7b4439f16a1
Matt Gibbs <matt.gibbs@hpe.com>
2017-02-05 11:20:54 -0500

    Remove example HelloWorld component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import {Hello} from './components/hello';
 
 import '../style/style.scss';
 
 ReactDOM.render(
-    <Hello compiler="Typescript" framework="React" />,
-    document.getElementById('example')
 );
