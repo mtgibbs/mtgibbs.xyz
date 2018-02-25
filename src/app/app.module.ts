b8ec7d8711dda8b7865578962c1a17b30359a38b
Matt Gibbs <matt@mtgibbs.xyz>
2018-02-25 18:21:09 -0500

    continue adding ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -20,7 +21,8 @@ import { DevIconListComponent } from './dev-icon-list/dev-icon-list.component';
     DevIconListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
