24848a119fccb45cf0ba1c9e9b13998792d316b5
Matt Gibbs <matt@mtgibbs.xyz>
2021-04-06 12:50:28 -0400

    Initial Commit ng 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
-import { IExperienceItem } from './experience-item/experience-item.component';
-import { IProfessionalConnectionLink } from './professional-connection-link/professional-connection-link.component';
-import { IDevIconOptions } from './dev-icon/dev-icon.component';
-
-import { EXPERIENCE } from './data/experience-items';
-import { LINKS } from './data/professional-links';
-import { EDUCATION } from './data/education-items';
-import { ICON_OPTIONS } from './data/skills-icons';
-import { INTERESTS } from './data/interest-items';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content">
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+      <span style="display: block">{{ title }} app is running!</span>
+      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    </div>
+    <h2>Here are some links to help you start: </h2>
+    <ul>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+      </li>
+    </ul>
+    
+  `,
+  styles: []
 })
 export class AppComponent {
-  
-  title = 'mtgibbs.xyz';
-
-  public experience: IExperienceItem[] = EXPERIENCE;
-  public links: IProfessionalConnectionLink[] = LINKS;
-  public education: IExperienceItem[] = EDUCATION;
-  public skillIcons: IDevIconOptions[] = ICON_OPTIONS;
-  public interests: IExperienceItem[] = INTERESTS;
+  title = 'mtgibbs-xyz';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ExperienceItemComponent } from './experience-item/experience-item.component';
-import { ExperienceItemListComponent } from './experience-item-list/experience-item-list.component';
-import { ProfessionalConnectionLinkComponent } from './professional-connection-link/professional-connection-link.component';
-import { DevIconComponent } from './dev-icon/dev-icon.component';
-import { DevIconListComponent } from './dev-icon-list/dev-icon-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    ExperienceItemComponent,
-    ExperienceItemListComponent,
-    ProfessionalConnectionLinkComponent,
-    DevIconComponent,
-    DevIconListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgbModule.forRoot()
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
