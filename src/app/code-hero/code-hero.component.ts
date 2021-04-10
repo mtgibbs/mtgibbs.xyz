37f1f86ec040f906a8294f460764557421be12ad
Matt Gibbs <matt@mtgibbs.xyz>
2021-04-10 11:28:36 -0400

    Adding some custom colors

--- src/app/code-hero/code-hero.component.ts
+++ src/app/code-hero/code-hero.component.ts
@@ -6,14 +6,14 @@ import { Component, Input, OnInit } from '@angular/core';
   template: `
 
     <div class="container relative mx-auto">
-      <div class="absolute z-0 inset-0 bg-gradient-to-r from-purple-400 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+      <div class="absolute z-0 inset-0 bg-gradient-to-r from-teal-lightest to-indigo-light shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
       <div class="relative z-10 h-96 p-4 sm:rounded-3xl  bg-gray-800 border-gray-900 shadow-md ">
         <div class="object-cover font-mono text-gray-200 h-full py-4 overflow-scroll scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-transparent scrollbar-thumb-rounded select-none">
           <mtg-code-hero-text [codeText]="codeText" [characterPointerLocation]="index"></mtg-code-hero-text>
         </div>
       </div>
       <div *ngIf="titleText" class="absolute w-full inset-y-1/2 h-9 object-center text-center z-20">
-        <span class="bg-gray-900 text-2xl text-purple-300 font-bold p-8 sm:rounded-3xl border-4 border-purple-300">{{ titleText }}</span>
+        <span class="bg-gray-900 text-2xl text-teal-light font-bold p-8 sm:rounded-3xl border-4 border-teal-light">{{ titleText }}</span>
       </div>
     </div>
    
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,32 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+   
+    extend: {
+      colors: {
+        teal: {
+          darkest: '#003133',
+          dark: '#0D4A4D',
+          DEFAULT: '#0D4A4D',
+          light: '#0D4A4D',
+          lightest: '#417E80',
+        },
+        indigo: {
+          darkest: '#67989A',
+          dark: '#162955',
+          DEFAULT: '#2E4272',
+          light: '#4F628E',
+          lightest: '#7887AB',
+        },
+        blue: {
+          darkest: '#4F628E',
+          dark: '#4F628E',
+          DEFAULT: '#29506D',
+          light: '#496D89',
+          lightest: '#496D89',
+        }
+      },
+    },
   },
   variants: {
     scrollbar: ['rounded'],
