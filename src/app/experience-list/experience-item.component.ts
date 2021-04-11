926ed7e5159840448f5ccde16ee8a19d05e83d23
Matt Gibbs <matt@mtgibbs.xyz>
2021-04-11 16:28:24 -0400

    trying some titled backdrop cards

--- src/app/experience-list/experience-item.component.ts
+++ src/app/experience-list/experience-item.component.ts
@@ -6,14 +6,16 @@ import { IExperienceItem } from './model/experience-list-item';
     template: `
         <div class="experience-item-container">
             <div [attr.index]="index">
-                <div class="resume-content mr-auto">
+            <div class="absolute z-0 inset-0 bg-gradient-to-r from-orange-light to-orange shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-3 sm:rounded-md w-screen sm:w-full"></div>
+                <div class="experience-details relative bg-gray-100">
                     <h3 class="font-bold">{{ experienceItem.title }}</h3>
                     <div class="font-light">{{ experienceItem.subTitle }}</div>
                     <p class="text-lg p-4">{{ experienceItem.description }}</p>
+                    <div *ngIf="experienceItem.startDate && experienceItem.endDate" class="float-right relative">
+                        <span class="text-primary">{{ experienceItem.startDate }} - {{ experienceItem.endDate }}</span>
+                    </div>
                 </div>
-                <div *ngIf="experienceItem.startDate && experienceItem.endDate" class="float-right">
-                    <span class="text-primary">{{ experienceItem.startDate }} - {{ experienceItem.endDate }}</span>
-                </div>
+               
             </div>
         </div>
     `,
