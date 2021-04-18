b8d73ae0d87b5d05891488363e5e5fd261ee9764
Matt Gibbs <matt@mtgibbs.xyz>
2021-04-18 12:03:35 -0400

    Fix issue with overflow making background slant disappear

--- src/app/code-hero/code-hero.component.ts
+++ src/app/code-hero/code-hero.component.ts
@@ -6,19 +6,22 @@ import { WindowService } from '../window.service';
   selector: 'mtg-code-hero',
   template: `
 
-    <div aria-hidden role=”presentation” class="container relative mx-auto overflow-hidden">
+    <div aria-hidden role=”presentation” class="container relative mx-auto">
       <div class="absolute z-0 inset-0 bg-gradient-to-r from-orange to-indigo-light shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
       <div class="relative z-10 h-96 p-4 sm:rounded-3xl  bg-gray-800 border-gray-900 shadow-md ">
         <div class="object-cover font-mono text-gray-200 h-full py-4 overflow-scroll scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-transparent scrollbar-thumb-rounded select-none">
           <mtg-code-hero-text [codeText]="codeText" [characterPointerLocation]="index"></mtg-code-hero-text>
         </div>
       </div>
-      <div *ngIf="titleText" class="absolute w-full inset-y-1/2 h-9 object-center text-center z-20 transform -translate-x-24 -translate-y-6">
-        <span class="bg-gray-800 text-2xl text-orange font-bold p-8 border-4 border-orange sm:rounded-md tracking-wide whitespace-nowrap">{{ titleText }}</span>
+      <div class="absolute w-full h-64 overflow-hidden inset-y-12">
+        <div *ngIf="titleText" class="absolute w-full inset-y-1/2 h-9 object-center text-center z-20 transform -translate-x-24 -translate-y-6">
+          <span class="bg-gray-800 text-2xl text-orange font-bold p-8 border-4 border-orange sm:rounded-md tracking-wide whitespace-nowrap">{{ titleText }}</span>
+        </div>
+        <div *ngIf="secondText" class="absolute w-full inset-y-1/2 h-8 object-center text-center z-20 animate-pulse transform translate-x-24 translate-y-6 -rotate-12">
+          <span class="bg-gray-800 bg-opacity-25 backdrop-blur-xl text-2xl text-teal-lightest font-bold p-8 sm:rounded-0 border-4 border-teal-lightest tracking-wide whitespace-nowrap">{{ secondText }}</span>
+        </div>
       </div>
-      <div *ngIf="secondText" class="absolute w-full inset-y-1/2 h-8 object-center text-center z-20 animate-pulse transform translate-x-24 translate-y-6 -rotate-12">
-        <span class="bg-gray-800 bg-opacity-25 backdrop-blur-xl text-2xl text-teal-lightest font-bold p-8 sm:rounded-0 border-4 border-teal-lightest tracking-wide whitespace-nowrap">{{ secondText }}</span>
-      </div>
+     
     </div>
   `,
   styles: [
