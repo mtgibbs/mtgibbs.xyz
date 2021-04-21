5b9b283c4816fa43a69decef6827e01893f992aa
Matt Gibbs <matt@mtgibbs.xyz>
2021-04-21 03:26:39 -0400

    Update experience text

--- src/data/experience-items.constants.ts
+++ src/data/experience-items.constants.ts
@@ -11,17 +11,18 @@ export const EXPERIENCE: IExperienceItem[] = [
     {
         title: 'Lead Software Developer',
         subTitle: 'DataScan',
-        description: 'I was the lead UI Engineer responsible for establishing strong CI tool \
+        description: 'Lead UI Engineer responsible for establishing strong CI tool \
                         integration for code quality and consistency. Ensuring quality \
-                        offered me the opportunity to teach junior developers new technologies, \
-                        standards, and practices.',
+                        offered me the opportunity to teach fellow developers new technologies, \
+                        standards, and practices.  Time spent mentoring peers developed my passion \
+                        for coaching and growing talent.',
         startDate: 'June 2018',
         endDate: 'June 2020'
     },
     {
         title: 'Lead Software Engineer',
         subTitle: 'Fortify on Demand',
-        description: 'I was a lead engineer for Fortify\'s SaaS Product, Fortify on Demand.  I was responsible for design and development of \
+        description: 'Lead engineer for Fortify\'s SaaS Product, Fortify on Demand.  I was responsible for design and development of \
                       features to manage the state of application security portfolios across 65000 applications \
                       and over 20000 users.  Within Fortify, I championed code reuse across our various teams to help grow our inner-source initiative through shareable \
                       components and libraries.  I helped guide the team on our open-source engagement and CI integration for security analysis in its infancy.',
@@ -31,7 +32,7 @@ export const EXPERIENCE: IExperienceItem[] = [
     {
         title: 'Software Engineer',
         subTitle: 'InComm',
-        description: 'I was a lead developer of a team striving to implement a new Master Data Management platform to consolidate and validate company-wide data using Postgresql, Spring, and JSF Primefaces. \
+        description: 'Lead developer of a team striving to implement a new Master Data Management platform to consolidate and validate company-wide data using Postgresql, Spring, and JSF Primefaces. \
                       With this new system we were able to implement feature sets from fraud detection and prevention to an easy interface for the user to modify and update records \
                       in an understandable and validated UI.',
         startDate: 'July 2012',
@@ -40,13 +41,9 @@ export const EXPERIENCE: IExperienceItem[] = [
     {
         title: 'Application Developer',
         subTitle: 'Aflac',
-        description: 'I developed and maintained a legacy high-volume automated policy processing suite interfacing with DB2 Databases, IBM ASM, and COBOL via JNI and CICS.  All new development was written in Java running in IBM WebSphere, \
-                      leveraging existing and stable ASM modules where it was possible.  I was involved in several projects to increase straight-through processing to decrease work on the customer and to increase  \
-                      system agility for new product time-to-market. This role is special for having exposed me large and complicated legacy systems when I was just cutting my teeth as a new developer.  \
-                      So often we find that when we automate without the willingness to adapt our business practices.  And when that happens, the system will virtually represent the physical scheme that we were \
-                      trying to automate.  Learning to adapt one\'s thinking to sell an idea on how to make something more maintainable after it has worked "just fine" for the last seven years \
-                      is something that every engineer or developer needs to go through.  There lies in the depths of these programs the absolute core of a company\'s history, mistakes, and successes. \
-                      And most of that history has likely been forgotten or taken for granted.',
+        description: 'Developed and maintained a legacy high-volume automated policy processing suite interfacing with DB2 Databases, IBM ASM, and COBOL via JNI and CICS. \
+                      I was involved in several projects to increase straight-through processing to decrease work on the customer and to increase  \
+                      system agility for new product time-to-market.',
         startDate: 'June 2009',
         endDate: 'July 2012'
     }
