70467642ecf6836a00b0c3179d3db8ad2db27851
Matt Gibbs <matt@mtgibbs.xyz>
2026-01-02 17:30:47 -0500

    style: refactor technology list to 2-column grid and fix discord icon

--- components/dev-icon-list/dev-icon-list.tsx
+++ components/dev-icon-list/dev-icon-list.tsx
@@ -7,15 +7,16 @@ interface DevIconListProps {
 
 const DevIconList = ({ icons }: DevIconListProps): JSX.Element => {
     return (
-        <div className="p-24">
-            <div className="container mx-auto whitespace-normal leading-[5em] break-words h-full inline-block text-center text-transparent bg-clip-text bg-gradient-to-b from-signal-orange via-tracking-red to-chrome-blue">
+        <div className="px-4 py-12 sm:p-24">
+            <div className="container mx-auto grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-8 items-center justify-items-center text-transparent bg-clip-text bg-gradient-to-b from-signal-orange via-tracking-red to-chrome-blue">
                 {icons.map((iconOption) => (
-                    <DevIcon
-                        key={iconOption.icon}
-                        icon={iconOption.icon}
-                        isColor={iconOption.isColor}
-                        isWordmark={iconOption.isWordmark}
-                        style={iconOption.style}></DevIcon>
+                    <div key={iconOption.icon} className="flex items-center justify-center p-4">
+                        <DevIcon
+                            icon={iconOption.icon}
+                            isColor={iconOption.isColor}
+                            isWordmark={iconOption.isWordmark}
+                            style={iconOption.style}></DevIcon>
+                    </div>
                 ))}
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const Home: NextPage = () => {
               Email <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
             </a>
             <a className="text-2xl mx-4 my-1 text-faded-cardboard hover:text-white transition-colors flex items-center gap-2" href="https://discordapp.com/users/pwnysenpai#6317/">
-              Discord <i className="devicon-discord-plain"></i>
+              Discord <i className="devicon-discord-original"></i>
             </a>
           </div>
 
