aa093cefb8ca1e80d902273ac9cba376c59ccb49
Matt Gibbs <matt@mtgibbs.xyz>
2026-01-04 17:25:44 -0500

    Style: Update Code Hero shadow to Chrome Blue (#55)
    * style(hero): update drop shadow to tighter blue block
    
    * style(hero): change border color to phosphor-amber
    
    * style(hero): change second text color to faded-cardboard
    
    * style(hero): reduce opacity of second text to 85%

--- components/code-hero/code-hero.tsx
+++ components/code-hero/code-hero.tsx
@@ -78,7 +78,7 @@ const CodeHero = ({ titleText, secondText }: CodeHeroProps): React.ReactNode => 
                 </div>
                 <div className={cn(
                     "relative z-10 h-96 p-4 sm:mx-2 md:mx-4 lg:mx-6 sm:rounded-none overflow-hidden",
-                    "bg-magnetic-black/90 backdrop-blur-sm border-4 border-faded-cardboard shadow-[10px_10px_0px_0px_rgba(0,0,0,1)]",
+                    "bg-magnetic-black/90 backdrop-blur-sm border-4 border-phosphor-amber shadow-[6px_6px_0px_0px_#3B5C7D]",
                     styles.crt,
                     globalVhsActive && styles.vhsSync
                 )}>
@@ -98,7 +98,7 @@ const CodeHero = ({ titleText, secondText }: CodeHeroProps): React.ReactNode => 
                     }
                     {secondText &&
                         <div className="relative w-full text-center z-20 transform mt-4 sm:mt-0 sm:translate-x-24 sm:translate-y-6 rotate-0 sm:-rotate-12" >
-                            <span className="text-sm sm:text-3xl text-phosphor-amber font-bold tracking-wide lazer84">
+                            <span className="text-sm sm:text-3xl text-faded-cardboard/85 font-bold tracking-wide lazer84">
                                 <span className="glitch relative inline-block" data-text={secondText}>{secondText}</span>
                             </span>
                         </div>
